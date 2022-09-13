939e0c29be8d5ce35fd6a3c380e1e6514951ded7
unknown <zagvinizag@gmail.com>
2022-09-13 20:42:03 -0300

    Organizando

---
+++ JavaGeneric/src/aula02/CriadorDeTabuada.java
@@ -0,0 +1,15 @@
+package aula02;
+
+import ferramentas.Entrada;
+
+import ferramentas.Func;
+
+public class CriadorDeTabuada {
+
+    public static void main(String[] args) {
+        
+        Func.tabuada();
+        
+        System.exit(0);
+    }
+}
