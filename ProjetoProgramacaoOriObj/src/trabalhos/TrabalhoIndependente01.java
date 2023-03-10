a45b2b97541115850489ec236f6686d4877f650c
Vinícius <zagvinizag@gmail.com>
2023-03-10 20:12:04 -0300

    Atividade 01

---
+++ ProjetoProgramacaoOriObj/src/trabalhos/TrabalhoIndependente01.java
@@ -0,0 +1,27 @@
+package trabalhos;
+
+import tools.Entrada;
+
+public class TrabalhoIndependente01 {
+
+    public static int criaVetorDe100() {
+        int [] vetor = new int[100];
+        int pares = 0;
+        int contador = 1;
+        for (int x = 1; x < vetor.length; x++) {
+            contador ++;
+            vetor[x] = (int) (Math.random()*500);
+            if (vetor[x] % 2 == 0) {
+                pares++;
+            }
+            System.out.print(vetor[x]+"\t");
+        }
+        System.out.println("");
+        System.out.println(contador + ": numeros foram gerados");
+        return pares;
+    }
+
+    public static void main(String[] args) {
+        Entrada.escrever("O vetor possui: " + criaVetorDe100() + ", numeros pares.");
+    }
+}
